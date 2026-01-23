58414cdbf37c20d002fd9debca6dc444cdc0420d
phantom787 <gitaudave14@gmail.com>
2026-01-23 10:56:25 -0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
 import StoryForm from "../components/StoryForm";
-import CategoryFilter from "../components/CategoryFilter";
 import "../index.css";
 
 export default function Home() {
   const [stories, setStories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
 
   const fetchStories = async () => {
     try {
       const res = await api.get("/stories");
-      setStories(res.data.reverse());
+      setStories(res.data.reverse()); // newest first
     } catch (err) {
       console.error(err);
     }
@@ -20,11 +18,6 @@ export default function Home() {
   useEffect(() => {
     fetchStories();
   }, []);
-
-  const categories = Array.from(new Set(stories.map((s) => s.category).filter(Boolean)));
-  const filteredStories = selectedCategory
-    ? stories.filter((s) => s.category === selectedCategory)
-    : stories;
 
   return (
     <div>
@@ -37,22 +30,14 @@ export default function Home() {
       {/* Story Form */}
       <StoryForm onStorySubmitted={fetchStories} />
 
-      {/* Category Filter */}
-      {categories.length > 0 && (
-        <CategoryFilter
-          categories={categories}
-          selectedCategory={selectedCategory}
-          onChange={setSelectedCategory}
-        />
-      )}
-
       {/* Stories Feed */}
       <section className="stories-feed">
-        {filteredStories.length === 0 && <p style={{ textAlign: "center", color: "#c4f0b0" }}>No stories yet!</p>}
-        {filteredStories.map((story) => (
+        {stories.length === 0 && (
+          <p style={{ textAlign: "center", color: "#c4f0b0" }}>No stories yet!</p>
+        )}
+        {stories.map((story) => (
           <div key={story.id} className="story-card">
             <p>{story.content}</p>
-            {story.category && <span>{story.category}</span>}
           </div>
         ))}
       </section>
