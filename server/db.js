2dbb99a70ef56106cfdf5177c1ccab99145504e9
phantom787 <gitaudave14@gmail.com>
2026-02-05 05:45:00 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
+// db.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }
 });
